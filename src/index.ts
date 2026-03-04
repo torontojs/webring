6372ab9f34055453002a0fcc291ff37c48f03fa4
Marco Campos <609904+madcampos@users.noreply.github.com>
2026-03-04 18:03:43 -0500

    feat: simplify and update code for routes

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,94 @@
 import { env } from 'cloudflare:workers';
-import { nextPage, previousPage, randomPage } from './routes.ts';
+import membersList from './members.json' with { type: 'json' };
+
+function getRequestInfo(request: Request) {
+	const originHeader = request.headers.get('Referer') ?? request.headers.get('Origin') ?? undefined;
+	const origin = URL.canParse(originHeader ?? '') ? new URL(originHeader ?? '') : undefined;
+
+	return {
+		requestUrl: new URL(request.url),
+		origin,
+		protocol: origin?.protocol.replace(':', '') ?? 'https',
+		index: origin ? membersList.members.findIndex(({ url: candidateUrl }) => new URL(candidateUrl).origin === origin.origin) : -1
+	};
+}
+
+function filterMemberList(protocol: string, origin?: URL) {
+	return membersList.members.filter(({ url }) => {
+		const candidateUrl = new URL(url);
+
+		return candidateUrl.protocol === protocol && candidateUrl.origin !== origin?.origin;
+	});
+}
+
+function nextPage(request: Request) {
+	const { origin, index, requestUrl, protocol } = getRequestInfo(request);
+
+	if (!origin || index === -1) {
+		return new Response(null, {
+			status: 307,
+			headers: new Headers({ Location: requestUrl.origin })
+		});
+	}
+
+	const filteredList = filterMemberList(protocol);
+	const nextPage = index + 1 === filteredList.length ? 0 : index + 1;
+
+	return new Response(null, {
+		status: 307,
+		headers: new Headers({ Location: filteredList.at(nextPage)!.url })
+	});
+}
+
+function previousPage(request: Request) {
+	const { origin, index, requestUrl, protocol } = getRequestInfo(request);
+
+	if (!origin || index === -1) {
+		return new Response(null, {
+			status: 307,
+			headers: new Headers({ Location: requestUrl.origin })
+		});
+	}
+
+	const filteredList = filterMemberList(protocol);
+
+	return new Response(null, {
+		status: 307,
+		headers: new Headers({ Location: filteredList.at(index - 1)!.url })
+	});
+}
+
+function randomPage(request: Request) {
+	const { protocol, origin, requestUrl } = getRequestInfo(request);
+	const filteredList = filterMemberList(protocol, origin);
+
+	const randomIndex = Math.floor(Math.random() * filteredList.length);
+
+	return new Response(null, {
+		status: 307,
+		headers: new Headers({ Location: filteredList.at(randomIndex)?.url ?? requestUrl.origin })
+	});
+}
 
 async function fetchHandler(request: Request) {
 	const url = new URL(request.url);
+
+	console.log(url);
 
 	let response: Response;
 
 	switch (url.pathname) {
 		case '/prev':
-			{
-				response = await previousPage(request);
-			}
+		case '/prev/':
+			response = previousPage(request);
 			break;
 		case '/next':
-			{
-				response = await nextPage(request);
-			}
+		case '/next/':
+			response = nextPage(request);
 			break;
 		case '/random':
-			{
-				response = await randomPage(request);
-			}
+		case '/random/':
+			response = randomPage(request);
 			break;
 		default: {
 			const errorPage = await env.Assets.fetch('https://assets.local/404.html');
